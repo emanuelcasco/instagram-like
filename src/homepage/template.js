404722a669bc85b2e88f9b1ca7cfac66d3f949df
Emanuel <cascoemanuel@gmail.com>
2017-04-19 19:34:12 -0300

    Fix

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -20,7 +20,7 @@ module.exports = function (pictures) {
       </div>
     </div>
     <div class="row">
-      <div class="col s12 m10 offset-m1 l6 offset-l3">
+      <div class="col s12 m10 offset-m1 l8 offset-l2">
         ${pictures.map(function (pic) {
           return picture(pic);
         })}
